fbd62339fccf84aa2906053c087dbf567690c093
Giannis Vasilopoulos <giannisvas123@gmail.com>
2020-09-20 14:51:52 +0300

    add fold typing animation & tech logos

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,14 @@ import Fold from "../src/pageComponents/Home/Fold/";
 import { getHome } from "../src/lib/contentful";
 
 function Home({ homeData }) {
+  const technologies = homeData.technologies.map((t) => t.fields);
   return (
     <MainLayout meta={homeData.metaData}>
-      <Fold />
+      <Fold
+        title={homeData.title}
+        typingTitles={homeData.typingTitles}
+        technologies={technologies}
+      />
       <div>
         <h1>{homeData.title}</h1>
       </div>
--- src/pageComponents/Home/Fold/index.jsx
+++ src/pageComponents/Home/Fold/index.jsx
@@ -1,8 +1,54 @@
 import styled from "@emotion/styled";
 import { keyframes } from "@emotion/core";
+import Typed from "react-typed";
 
-function Fold() {
-  return <div>Fold</div>;
+const FoldContainer = styled.div({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  position: "relative",
+  height: "100vh",
+  color: "#fff",
+  fontSize: "2.8em",
+  background: "#281d1d",
+  alignItems: "center",
+  padding: "0 15px",
+});
+
+const TechnologiesContainer = styled.div({
+  display: "flex",
+  justifyContent: "space-between",
+  maxWidth: 625,
+  padding: "20px 15px 0",
+  width: "100%",
+});
+
+const Logo = styled.img({
+  maxWidth: "50px",
+});
+
+function Fold({ title, typingTitles, technologies }) {
+  return (
+    <FoldContainer id="home">
+      {title}
+      <Typed
+        strings={typingTitles}
+        start-delay={2000}
+        typeSpeed={100}
+        backSpeed={100}
+        cursor-char="'|'"
+        backDelay={1}
+        loop
+        loopCount={3}
+        smartBackspace
+      />
+      <TechnologiesContainer>
+        {technologies.map((tech) => {
+          return <Logo src={tech.file.url} alt={tech.title} key={tech.title} />;
+        })}
+      </TechnologiesContainer>
+    </FoldContainer>
+  );
 }
 
 export default Fold;
