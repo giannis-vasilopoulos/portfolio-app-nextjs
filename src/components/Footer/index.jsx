6d47999d78d9c8198efee3a05576b9035ec6c5b1
Giannis Vasilopoulos <giannisvas123@gmail.com>
2021-02-13 18:15:31 +0200

    intersection observer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -7,36 +7,34 @@ import {
   CopyrightText,
 } from "./footer.styles";
 
-import { Slide } from "react-awesome-reveal";
-
 function Footer({ footer, id }) {
   return (
     <ContactContainer id={id}>
-      <Slide direction="up" triggerOnce>
-        <SocialContainer>
-          <h2>{footer.contactTitle}</h2>
-          <SocialList>
-            {footer.socialMenu.map(({ fields: social }) => {
-              return (
-                <li key={social.title}>
-                  <a
-                    href={social.link}
-                    target="_blank"
-                    rel="noreferrer"
-                    dangerouslySetInnerHTML={{ __html: social.svgCode }}
-                  />
-                </li>
-              );
-            })}
-          </SocialList>
-        </SocialContainer>
-      </Slide>
+      <SocialContainer>
+        <h2>{footer.contactTitle}</h2>
+        <SocialList>
+          {footer.socialMenu.map(({ fields: social }) => {
+            return (
+              <li key={social.title}>
+                <a
+                  href={social.link}
+                  target="_blank"
+                  rel="noreferrer"
+                  dangerouslySetInnerHTML={{ __html: social.svgCode }}
+                />
+              </li>
+            );
+          })}
+        </SocialList>
+      </SocialContainer>
       <CopyrightContainer>
         <CopyrightText>
           © {new Date().getFullYear()} {footer.copyright}{" "}
           <VersionStyle>{process.env.NEXT_PUBLIC_VERSION}</VersionStyle>
         </CopyrightText>
-        <CopyrightText align="right" gutterMb>{footer.createdLabel}</CopyrightText>
+        <CopyrightText align="right" gutterMb>
+          {footer.createdLabel}
+        </CopyrightText>
       </CopyrightContainer>
     </ContactContainer>
   );
--- src/pageComponents/Home/About/index.jsx
+++ src/pageComponents/Home/About/index.jsx
@@ -2,6 +2,8 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Skills from "components/Skills/";
+import { motion, useViewportScroll } from "framer-motion";
+
 import {
   AboutContainer,
   Title,
@@ -13,7 +15,6 @@ import {
   ToolImage,
   ColStyles,
 } from "./about.styles";
-import { Slide } from "react-awesome-reveal";
 
 function About({
   title,
@@ -24,13 +25,15 @@ function About({
   toolsTitle,
   toolsImages,
 }) {
+  const { scrollY } = useViewportScroll();
+  console.log(scrollY);
   return (
-    <AboutContainer id="about">
-      <Container>
-        <Title>{title}</Title>
-        <Row css={RowStyles} className="align-items-center">
-          <Col md="6" css={ColStyles}>
-            <Slide direction="left" triggerOnce>
+    <motion.div style={{ top: scrollY }}>
+      <AboutContainer id="about">
+        <Container>
+          <Title>{title}</Title>
+          <Row css={RowStyles} className="align-items-center">
+            <Col md="6" css={ColStyles}>
               <ImageBio
                 src={aboutImage.file.url}
                 alt={aboutImage.file.fileName.replace(".jpg", "")}
@@ -38,38 +41,36 @@ function About({
               <BioContainer
                 dangerouslySetInnerHTML={{ __html: bio.content[0].value }}
               />
-            </Slide>
-          </Col>
-          <Col md="6">
-            <Slide direction="right" triggerOnce>
+            </Col>
+            <Col md="6">
               <h3>{skillsTitle}</h3>
               <Skills skills={skillsItems} />
-            </Slide>
-          </Col>
-        </Row>
-        <ToolsContainer>
-          <h3>{toolsTitle}</h3>
-          <Row css={ToolsRowStyles} className="align-items-center">
-            {toolsImages.map(({ fields: image }) => {
-              return (
-                <Col
-                  key={image.title}
-                  xs="6"
-                  lg={image.description > 90 ? "3" : "2"}
-                  className="mb-4"
-                >
-                  <ToolImage
-                    alt={image.title}
-                    src={image.file.url}
-                    width={image.description}
-                  />
-                </Col>
-              );
-            })}
+            </Col>
           </Row>
-        </ToolsContainer>
-      </Container>
-    </AboutContainer>
+          <ToolsContainer>
+            <h3>{toolsTitle}</h3>
+            <Row css={ToolsRowStyles} className="align-items-center">
+              {toolsImages.map(({ fields: image }) => {
+                return (
+                  <Col
+                    key={image.title}
+                    xs="6"
+                    lg={image.description > 90 ? "3" : "2"}
+                    className="mb-4"
+                  >
+                    <ToolImage
+                      alt={image.title}
+                      src={image.file.url}
+                      width={image.description}
+                    />
+                  </Col>
+                );
+              })}
+            </Row>
+          </ToolsContainer>
+        </Container>
+      </AboutContainer>
+    </motion.div>
   );
 }
 
