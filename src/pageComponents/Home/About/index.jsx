cb8bef6b729aa3d93eb88229c880a7c0a42e8b1b
Giannis Vasilopoulos <giannisvas123@gmail.com>
2021-02-13 20:45:54 +0200

    add animation using Intersection Observer package

--- src/pageComponents/Home/About/index.jsx
+++ src/pageComponents/Home/About/index.jsx
@@ -2,7 +2,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Skills from "components/Skills/";
-import { motion, useViewportScroll } from "framer-motion";
+import { InView } from "react-intersection-observer";
 
 import {
   AboutContainer,
@@ -12,6 +12,8 @@ import {
   ToolsRowStyles,
   ImageBio,
   BioContainer,
+  LeftColStyles,
+  RightColStyles,
   ToolImage,
   ColStyles,
 } from "./about.styles";
@@ -25,52 +27,66 @@ function About({
   toolsTitle,
   toolsImages,
 }) {
-  const { scrollY } = useViewportScroll();
-  console.log(scrollY);
   return (
-    <motion.div style={{ top: scrollY }}>
-      <AboutContainer id="about">
-        <Container>
-          <Title>{title}</Title>
-          <Row css={RowStyles} className="align-items-center">
-            <Col md="6" css={ColStyles}>
-              <ImageBio
-                src={aboutImage.file.url}
-                alt={aboutImage.file.fileName.replace(".jpg", "")}
-              />
-              <BioContainer
-                dangerouslySetInnerHTML={{ __html: bio.content[0].value }}
-              />
-            </Col>
-            <Col md="6">
-              <h3>{skillsTitle}</h3>
-              <Skills skills={skillsItems} />
-            </Col>
-          </Row>
-          <ToolsContainer>
-            <h3>{toolsTitle}</h3>
-            <Row css={ToolsRowStyles} className="align-items-center">
-              {toolsImages.map(({ fields: image }) => {
-                return (
-                  <Col
-                    key={image.title}
-                    xs="6"
-                    lg={image.description > 90 ? "3" : "2"}
-                    className="mb-4"
-                  >
-                    <ToolImage
-                      alt={image.title}
-                      src={image.file.url}
-                      width={image.description}
+    <AboutContainer id="about">
+      <Container>
+        <Title>{title}</Title>
+
+        <Row css={RowStyles} className="align-items-center">
+          <Col md="6" css={ColStyles}>
+            <InView threshold={0.5} triggerOnce={true}>
+              {({ ref, inView }) => (
+                <div ref={ref}>
+                  <LeftColStyles inView={inView}>
+                    <ImageBio
+                      src={aboutImage.file.url}
+                      alt={aboutImage.file.fileName.replace(".jpg", "")}
                     />
-                  </Col>
-                );
-              })}
-            </Row>
-          </ToolsContainer>
-        </Container>
-      </AboutContainer>
-    </motion.div>
+                    <BioContainer
+                      dangerouslySetInnerHTML={{ __html: bio.content[0].value }}
+                    />
+                  </LeftColStyles>
+                </div>
+              )}
+            </InView>
+          </Col>
+          <Col md="6">
+            <InView threshold={0.5} triggerOnce={true}>
+              {({ ref, inView }) => (
+                <div ref={ref}>
+                  <RightColStyles inView={inView}>
+                    <h3>{skillsTitle}</h3>
+                    <Skills skills={skillsItems} />
+                  </RightColStyles>
+                </div>
+              )}
+            </InView>
+          </Col>
+        </Row>
+
+        <ToolsContainer>
+          <h3>{toolsTitle}</h3>
+          <Row css={ToolsRowStyles} className="align-items-center">
+            {toolsImages.map(({ fields: image }) => {
+              return (
+                <Col
+                  key={image.title}
+                  xs="6"
+                  lg={image.description > 90 ? "3" : "2"}
+                  className="mb-4"
+                >
+                  <ToolImage
+                    alt={image.title}
+                    src={image.file.url}
+                    width={image.description}
+                  />
+                </Col>
+              );
+            })}
+          </Row>
+        </ToolsContainer>
+      </Container>
+    </AboutContainer>
   );
 }
 
