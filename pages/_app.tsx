22186b3237ff90bd453ba8c0ed49c03734c6bf22
Giannis Vasilopoulos <giannisvas123@gmail.com>
2022-01-31 22:22:27 +0200

    transform app.js to tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
+import type { AppProps } from "next/app";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "styles/globals.css";
 
-function PortfolioApp({ Component, pageProps }) {
+function PortfolioApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
