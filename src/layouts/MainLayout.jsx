162bba1cba94fb97003e9827df867bdcf01735f3
Giannis Vasilopoulos <giannisvas123@gmail.com>
2022-12-20 21:01:16 +0200

    add comments to disable next/script

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable @next/next/next-script-for-ga */
+/* eslint-disable @next/next/no-sync-scripts */
 import Head from "next/head";
 import Loader from "components/Loader/";
 import Footer from "components/Footer";
