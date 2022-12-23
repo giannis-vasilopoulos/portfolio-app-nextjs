9fac38b3b1439e081a3dd2faa40b14211f4b353e
Giannis Vasilopoulos <giannisvas123@gmail.com>
2022-12-23 18:21:38 +0200

    add eslint configs and minor changes

--- src/pageComponents/Home/About/about.styles.js
+++ src/pageComponents/Home/About/about.styles.js
@@ -3,62 +3,63 @@ import { css } from "@emotion/react";
 
 export const AboutContainer = styled.div({
   minHeight: "100vh",
-  padding: "100px 0",
+  padding: "100px 0"
 });
 
 export const Title = styled.h2({
   textAlign: "center",
-  textTransform: "uppercase",
+  textTransform: "uppercase"
 });
 
 export const RowStyles = css({
-  marginTop: "4.5em",
+  marginTop: "4.5em"
 });
 
 export const ToolsRowStyles = css({
   justifyContent: "space-around",
   maxWidth: 700,
-  margin: "auto",
+  margin: "auto"
 });
 
 export const ImageBio = styled.img({
   width: 250,
   borderRadius: "20%",
   height: 250,
-  objectFit: "cover",
+  objectFit: "cover"
 });
 
 export const ColStyles = css({
-  textAlign: "center",
+  textAlign: "center"
 });
 
 export const LeftColStyles = styled.div(
   {
-    transition: "all ease 0.7s",
+    transition: "all ease 0.7s"
   },
   (props) => ({
-    transform: props.inView ? "translateX(0)" : "translateX(-50vw)",
+    transform: props.inView ? "translateX(0)" : "translateX(-50vw)"
   })
 );
 
 export const RightColStyles = styled.div(
   {
-    transition: "all ease 0.7s",
+    transition: "all ease 0.7s"
   },
   (props) => ({
-    transform: props.inView ? "translateX(0)" : "translateX(50vw)",
+    transform: props.inView ? "translateX(0)" : "translateX(50vw)"
   })
 );
 
 export const BioContainer = styled.div({
-  marginTop: "1em",
+  maxWidth: "75%",
+  margin: "1em auto 0"
 });
 
 export const ToolsContainer = styled.div({
   marginTop: "3em",
-  textAlign: "center",
+  textAlign: "center"
 });
 
 export const ToolImage = styled.img((props) => ({
-  width: props.width ? props.width : 90,
+  width: props.width ? props.width : 90
 }));
