df31c31d5f225175ddee5b597a7e71c2ca538342
Giannis Vasilopoulos <giannisvas123@gmail.com>
2023-02-07 00:25:30 +0200

    update danger

--- dangerfile.js
+++ dangerfile.js
@@ -7,7 +7,7 @@ message("Changed Files in this PR: \n - " + modifiedMD);
 const { additions = 0, deletions = 0 } = danger.github.pr;
 message(`:tada: The PR added ${additions} and removed ${deletions} lines.`);
 
-const bigPRThreshold = 600;
+const bigPRThreshold = 1500;
 let errorCount = 0;
 if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
   warn(":exclamation: Big PR (" + ++errorCount + ")");
@@ -16,4 +16,6 @@ if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
       errorCount +
       ") : Pull Request size seems relatively large. If Pull Request contains multiple changes, split each into separate PR will helps faster, easier review."
   );
+} else {
+  message("This PR looks fine and easy for code review");
 }
