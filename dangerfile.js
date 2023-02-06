a0cd9f8a0af439a1828a27273502085f80ba02d3
Giannis Vasilopoulos <giannisvas123@gmail.com>
2023-02-06 23:25:30 +0200

    test danger plugin

--- dangerfile.js
+++ dangerfile.js
@@ -1,4 +1,8 @@
-import { message, danger } from "danger";
+import { message, danger, schedule } from "danger";
+import npmOutdated from "danger-plugin-npm-outdated";
+
+// Note: You need to use schedule()
+schedule(npmOutdated());
 
 const modifiedMD = danger.git.modified_files.join("- ");
 message("Changed Files in this PR: \n - " + modifiedMD);
